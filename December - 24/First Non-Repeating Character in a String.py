f6f22cc373b5092e5bb16281160d28eac32c8275
DevilCreeper-0906 <harvigu06@gmail.com>
2026-01-04 22:39:36 +0530

    Create First Non-Repeating Character in a String.py

---
+++ December - 24/First Non-Repeating Character in a String.py
@@ -0,0 +1,19 @@
+s = input("Enter the string: ")
+
+freq = {}
+
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+found = False
+for ch in s:
+    if freq[ch] == 1:
+        print("The first non-repeating character is:", ch)
+        found = True
+        break
+
+if not found:
+    print("No non-repeating character found.")
